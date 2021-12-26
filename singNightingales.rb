349a2a8537c31c5c4301b1730ecb53acf124d172
my-app-s <94853425+my-app-s@users.noreply.github.com>
2021-12-26 23:41:09 +0600

    Add files via upload

---
+++ singNightingales.rb
@@ -0,0 +1,16 @@
+puts "Birds song"
+degrees, season = ARGV[0], ARGV[1]
+
+if degrees == nil and season == nil
+    puts "What temperature is it now?"
+    degrees = STDIN.gets.chomp
+
+    puts "What season? (0 - spring, 1 - summer, 2 - autumn, 3 - winter)"
+    season = STDIN.gets.chomp
+end
+
+if (degrees >= "15" or degrees <= "35") and season == "1"
+    puts "Rather, go to the park, the nightingales are singing!"
+elsif degrees >= "22" or  degrees <= "30"
+    puts "Now the nightingales are silent, warming themselves or cooling off!)"
+end
